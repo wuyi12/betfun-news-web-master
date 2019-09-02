665ad4204113d34b3af1777c9b42c45b3f0ab487
wuyi12 <1158648551@qq.com>
2019-09-02 15:11:40 +0800

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div class="header">
+      <p><span style="color:blue;">风险提示：</span>谨防ICO、变相ICO 丨 观索所发表资讯不代表本公司任何投资暗示。</p>
+    </div>
+    <el-row>
+  <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+  <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+  <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+</el-row>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import ElementUI from 'element-ui'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: '女程序员'
     }
   }
 }
@@ -17,4 +25,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+*{margin: 0;padding: 0;}
+.hello .header{background-color: #ccc;height: 19px; line-height: 19px; text-align: center;opacity:1;}
 </style>
